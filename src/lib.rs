155a8e6630944e1a61e5f1afff2365ca80fc2263
yisar <1533540012@qq.com>
2022-02-22 22:32:55 +0800

    日常提交

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,16 @@ extern crate wasm_bindgen;
 
 use wasm_bindgen::prelude::*;
 
+pub mod lexer;
+pub mod parser;
+pub mod generator;
+
 #[wasm_bindgen]
-pub fn greet(name: &str) -> String {
-    return format!("Hello, {}!", name);
+pub fn compile(str: &str) -> String {
+    let mut parser = parser::Parser::new(str);
+    let ast = parser.parse_all().unwrap();
+    let mut gen = generator::Generator::new(ast);
+    let code = gen.generate_fre();
+    println!("{:#?}", code);
+    return code;
 }
