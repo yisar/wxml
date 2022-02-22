716a6f30ec1c38c2b827328b642d1451b14d4d00
yisar <1533540012@qq.com>
2022-02-22 22:53:36 +0800

    emmm

--- docs/wxml_parser.js
+++ docs/wxml_parser.js
@@ -80,15 +80,15 @@ function getStringFromWasm0(ptr, len) {
     return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
 }
 /**
-* @param {string} name
+* @param {string} str
 * @returns {string}
 */
-export function greet(name) {
+export function compile(str) {
     try {
         const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
-        var ptr0 = passStringToWasm0(name, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+        var ptr0 = passStringToWasm0(str, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
         var len0 = WASM_VECTOR_LEN;
-        wasm.greet(retptr, ptr0, len0);
+        wasm.compile(retptr, ptr0, len0);
         var r0 = getInt32Memory0()[retptr / 4 + 0];
         var r1 = getInt32Memory0()[retptr / 4 + 1];
         return getStringFromWasm0(r0, r1);
